678f300355f306739a987b3f1d68d67387ed5ab6
Calebsenm <calebsenm@gmail.com>
2026-01-01 21:42:28 -0500

    first commit

---
+++ app/src/main/java/com/app/leelo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.app.leelo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.app.leelo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new TextFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.texts) {
+                replaceFragment(new TextFragment());
+            }
+            else if (item.getItemId() == R.id.words) {
+                replaceFragment(new WordsFragment());
+            }
+            else if (item.getItemId() == R.id.settings) {
+                replaceFragment(new SettingFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
+
