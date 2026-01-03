2af8ec5f54fe1512ea430f3b491299bc298b4ed4
Calebsenm <calebsenm@gmail.com>
2026-01-02 23:22:21 -0500

    add: practice window

---
+++ app/src/main/java/com/app/leelo/PracticeFragment.java
@@ -0,0 +1,24 @@
+package com.app.leelo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PracticeFragment extends Fragment {
+
+    public PracticeFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_practice, container, false);
+    }
+}
