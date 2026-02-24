a861d4d0c9fb5768993031c6fd520274abd40bf1
Calebsenm <calebsenm@gmail.com>
2026-02-23 21:58:04 -0500

    add: white and night mode

--- app/src/main/java/com/app/leelo/ui/MainActivity.java
+++ app/src/main/java/com/app/leelo/ui/MainActivity.java
@@ -1,54 +1,49 @@
 package com.app.leelo.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-//import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.app.leelo.R;
 import com.app.leelo.databinding.ActivityMainBinding;
+import com.app.leelo.util.ThemeManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-    // public static Bundle textUpdateData = new Bundle();
+    private ActivityMainBinding binding;
+    private ThemeManager themeManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        themeManager = ThemeManager.getInstance(this);
+        themeManager.applyTheme();
+        
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new TextFragment());
+        
+        if (savedInstanceState == null) {
+            replaceFragment(new TextFragment());
+        }
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-
             if (item.getItemId() == R.id.texts) {
                 replaceFragment(new TextFragment());
-            }
-            else if (item.getItemId() == R.id.words) {
+            } else if (item.getItemId() == R.id.words) {
                 replaceFragment(new WordsFragment());
-            }
-            else if (item.getItemId() == R.id.settings) {
+            } else if (item.getItemId() == R.id.settings) {
                 replaceFragment(new SettingFragment());
             }
-
             return true;
         });
     }
 
     public void replaceFragment(Fragment fragment) {
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
-        //fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
     }
-
 }
-
